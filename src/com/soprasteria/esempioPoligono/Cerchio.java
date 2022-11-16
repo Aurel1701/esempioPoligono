6741221c445655282b257b971272c109d7350fb6
TAFA Aurel <aurel.tafa@soprasteria.com>
2022-11-16 15:13:41 +0100

    add Cerchio and Interfaccia

---
+++ src/com/soprasteria/esempioPoligono/Cerchio.java
@@ -0,0 +1,14 @@
+package com.soprasteria.esempioPoligono;
+
+public class Cerchio implements Poligono {
+	
+   public float raggio;
+	
+	
+	public Cerchio (float raggio) {
+		this.raggio = raggio;
+	}
+	
+	
+
+}
---
+++ src/com/soprasteria/esempioPoligono/Poligono.java
@@ -0,0 +1,5 @@
+package com.soprasteria.esempioPoligono;
+
+public interface Poligono {
+
+}
