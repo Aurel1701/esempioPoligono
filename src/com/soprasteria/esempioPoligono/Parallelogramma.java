da89e540f260ddf7accf8a0fe166b6c980c1cebf
TAFA Aurel <aurel.tafa@soprasteria.com>
2022-11-16 16:36:15 +0100

    aggiunta Parallelogramma e calcolo Area

---
+++ src/com/soprasteria/esempioPoligono/Parallelogramma.java
@@ -0,0 +1,35 @@
+package com.soprasteria.esempioPoligono;
+
+public class Parallelogramma {
+	private float base;
+	private float altezza;
+	
+	public Parallelogramma(float base, float altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+
+	public float getBase() {
+		return base;
+	}
+
+	public void setBase(float base) {
+		this.base = base;
+	}
+
+	public float getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(float altezza) {
+		this.altezza = altezza;
+	}
+	
+	
+	public float calcoloArea() {
+		
+		return (float) (base*altezza);
+	}
+	
+
+}
